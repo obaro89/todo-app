52fb5d05905f2509225189a33c5cb5b5b430f0c3
obaro89 <igbinobaroosaretin@gmail.com>
2022-03-07 14:01:52 +0000

    redeployedtoheroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
 app.use(require("./api/routes/todo"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("view/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "view", "build", "index.html"));
   });
